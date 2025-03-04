10d329c86d33710ccf8bfb691f33320660a1af21
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-03-04 10:57:33 +0530

    added the repository layer for user

---
+++ src/repository/user-repository.js
@@ -0,0 +1,114 @@
+const User = require("../models/user");
+
+class UserRepository {
+  async createUser(user) {
+    try {
+      const user = await User.create(user);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer.");
+      throw error;
+    }
+  }
+
+  async getUser(userId) {
+    try {
+      const user = await User.findById(userId);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer.");
+      throw error;
+    }
+  }
+
+  async updateUser(userId, data) {
+    try {
+      const { firstName, lastName, color } = data;
+
+      if (!firstName || !lastName || color === null) {
+        throw new Error("First name, last name, and color are required");
+      }
+
+      if (typeof firstName !== "string" || typeof lastName !== "string") {
+        throw new Error("First name, last name, and color must be strings");
+      }
+      const user = await User.findByIdAndUpdate(
+        userId,
+        {
+          firstName,
+          lastName,
+          color,
+          profileSetup: true,
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer.");
+      throw error;
+    }
+  }
+
+  async destroyUser(userId) {
+    try {
+      const response = await User.findByIdAndDelete(userId);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer.");
+      throw error;
+    }
+  }
+
+  async findBy(data) {
+    try {
+      const response = await User.findOne(data);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateProfilePic(userId, fileName) {
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      user.image = fileName;
+      await user.save();
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer.");
+      throw error;
+    }
+  }
+
+  async removeProfilePic(userId) {
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        throw new Error("user not found");
+      }
+      if (user.image) {
+        try {
+          await unlink(user.image); // Delete the file
+        } catch (err) {
+          console.error("Error deleting image file:", err);
+        }
+      }
+      user.image = null;
+      await user.save();
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer.");
+      throw error;
+    }
+  }
+}
+
+module.exports = UserRepository;
